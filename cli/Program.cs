6acb835660fbe6cd5eca50c5a43727962b72ea9c
Matthew John Cheetham <mjcheetham@outlook.com>
2021-03-31 12:49:00 +0100

    Initial commit

---
+++ cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using wpfapp;
+
+namespace cli
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            await WpfAppUtils.ShowDialog();
+
+            Console.WriteLine("Goodbye World!");
+        }
+    }
+}
---
+++ wpfapp/WpfAppUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace wpfapp
+{
+    public static class WpfAppUtils
+    {
+        public static Task ShowDialog()
+        {
+            return StartStaTask(() =>
+            {
+                var window = new MainWindow();
+                window.ShowDialog();
+            });
+        }
+
+        private static Task StartStaTask(Action action)
+        {
+            var completionSource = new TaskCompletionSource<object>();
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    action();
+                    completionSource.SetResult(null);
+                }
+                catch (Exception e)
+                {
+                    completionSource.SetException(e);
+                }
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+
+            return completionSource.Task;
+        }
+    }
+}
